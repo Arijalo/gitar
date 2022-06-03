31dc707d9eb97ee2d30228bdd1b173a64c756325
Arijalo <33032464+Arijalo@users.noreply.github.com>
2022-06-04 06:04:37 +0700

    Update trans.js

--- trans.js
+++ trans.js
@@ -1,7 +1,7 @@
 var match;
 var chords =
-    ['C','C#','D','Eb','E','F','F#','G','Ab','A','Bb','B','C',
-     'Db','D','D#','E','F','Gb','G','G#','A','A#','C'];
+    ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B','C',
+     'C#','D','D#','E','F','G#','G','G#','A','A#','B','C'];
 var chordRegex = /C#|D#|F#|G#|A#|Db|Eb|Gb|Ab|Bb|C|D|E|F|G|A|B/g;
 
 $('#transposeUp').click(function() {
